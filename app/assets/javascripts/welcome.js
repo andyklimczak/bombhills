360a30c99caa6d184cb52b45d683999a9dac1de2
Andy Klimczak <andyklimczak@fastmail.com>
2015-10-09 18:50:21 +0000

    Initial commit - acts same as .zip file

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,103 @@
+// JavaScript Document
+var main = function() {
+  $(".button").mouseenter(function() {
+    $(this).toggleClass("activebutton");
+  });
+  $(".button").mouseleave(function() {
+    $(this).toggleClass("activebutton");
+  });
+  $('.openmap').mouseenter(function() {
+	  $(this).addClass("mapbuttonhighlight");
+  }); 
+  $('.openmap').mouseleave(function() {
+	  $(this).removeClass("mapbuttonhighlight");
+  }); 
+};
+
+
+function GetMap() {
+    Microsoft.Maps.loadModule('Microsoft.Maps.Themes.BingTheme', {
+        callback: function() {
+            map = new Microsoft.Maps.Map(document.getElementById(
+                'myMap'), {
+                credentials: 'Al9aBb7etkdbFTyp2LNeKvnuZsE7r1-gCm95LmXZru0rbfBTSNaQjRxd2WDatfii',
+                theme: new Microsoft.Maps.Themes.BingTheme()
+            });
+			
+            map.setView({
+                mapTypeId: Microsoft.Maps.MapTypeId.road,
+				zoom: 10,
+                center: new Microsoft.Maps.Location(37.776586, -122.448811)
+            });
+			
+			var newPoint = function(name, loc1, loc2, title, description) {
+    			name = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(loc1,
+        			loc2), null);
+    			map.entities.push(name);
+    			map.entities.push(new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(
+        			loc1, loc2), {
+        			title: title,
+        			description: description,
+       				pushpin: name
+    			}));
+			}
+			var sanfranlargest = new newPoint("sanfranlargest", 37.800309, -122.418117,
+    			"Filbert Street", "high density");
+			var thevalleyamazingtrail = new newPoint("thevalleyamazingtrail", 41.460184, -81.823116,
+    			"Amazing Trail", "watch out for bikers");
+        }
+    });
+}
+
+
+var setLocation = function() {
+    $('#findmebutton').click(function() {
+        var geoLocationProvider = new Microsoft.Maps.GeoLocationProvider(
+            map);
+        geoLocationProvider.getCurrentPosition();
+    });
+}
+
+var displayMap = function() {
+    $(function() {
+        $('#pinimage').click(function() {
+			$('#myMap').animate({
+                left: '0'
+            });
+			$('#myMap').animate({
+                opacity: '1'
+            });
+			$('video').addClass('videostop');
+			$('html').addClass('htmlbodycolor');
+			$('.controlsoff').addClass('controlson');
+			$('.citation').addClass('citationoff');
+			 $('.mymapoff').addClass('mymapon');
+        });
+    });
+}
+var closeMap = function() {
+    $('#closemapbutton').click(function() {
+        $('#myMap').animate({
+            opacity: '.3'
+        });
+		$('#myMap').animate({
+            left: '-2000px'
+        });
+		$('#myMap').animate({
+            opacity: '0'
+        });
+		$('video').removeClass('videostop');
+		$('html').removeClass('htmlbodycolor');
+		$('.controlsoff').removeClass('controlson');
+		$('.citation').removeClass('citationoff');
+		$('.mymapoff').removeClass('mymapon');
+    });
+};
+
+$(document).ready(displayMap);
+$(document).ready(closeMap);
+$(document).ready(setLocation);
+$(document).ready(main);
+$(document).ready(newPoint); 
+$(document).ready(GetMap); 
+
