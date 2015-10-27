a12c974eb2a282572cf6abf70cc3d56cd62898b3
alecs36 <alecs36@yahoo.com>
2015-10-27 19:16:27 -0400

    Changed layout / ui

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,16 +1,8 @@
 // JavaScript Document
+
 var main = function() {
-  $(".button").mouseenter(function() {
-    $(this).toggleClass("activebutton");
-  });
-  $(".button").mouseleave(function() {
-    $(this).toggleClass("activebutton");
-  });
-  $('.openmap').mouseenter(function() {
-	  $(this).addClass("mapbuttonhighlight");
-  }); 
-  $('.openmap').mouseleave(function() {
-	  $(this).removeClass("mapbuttonhighlight");
+  $('#maplistitemone').click(function() {
+    $('.addformoff').addClass("addformon");
   }); 
 };
 
@@ -31,16 +23,16 @@ function GetMap() {
             map = new Microsoft.Maps.Map(document.getElementById(
                 'myMap'), {
                 credentials: 'Al9aBb7etkdbFTyp2LNeKvnuZsE7r1-gCm95LmXZru0rbfBTSNaQjRxd2WDatfii',
-                theme: new Microsoft.Maps.Themes.BingTheme()
+                theme: new Microsoft.Maps.Themes.BingTheme(),enableSearchLogo: false, showMapTypeSelector:false,showDashboard: false
             });
-			
+      
             map.setView({
                 mapTypeId: Microsoft.Maps.MapTypeId.road, 
                 zoom: 10,
                 center: new Microsoft.Maps.Location(37.776586, -122.448811)
             });
 
-			
+      
           var sanfranlargest = new newPoint("sanfranlargest", 37.800309, -122.418117,
               "Filbert Street", "high density");
           var thevalleyamazingtrail = new newPoint("thevalleyamazingtrail", 41.460184, -81.823116,
@@ -64,36 +56,36 @@ var setLocation = function() {
 var displayMap = function() {
     $(function() {
         $('#pinimage').click(function() {
-			$('#myMap').animate({
+      $('#myMap').animate({
                 left: '0'
             });
-			$('#myMap').animate({
+      $('#myMap').animate({
                 opacity: '1'
             });
-			$('video').addClass('videostop');
-			$('html').addClass('htmlbodycolor');
-			$('.controlsoff').addClass('controlson');
-			$('.citation').addClass('citationoff');
-			 $('.mymapoff').addClass('mymapon');
+      $('video').addClass('videostop');
+      $('html').addClass('htmlbodycolor');
+      $('.controlsoff').addClass('controlson');
+      $('.citation').addClass('citationoff');
+       $('.mymapoff').addClass('mymapon');
         });
     });
 }
 var closeMap = function() {
-    $('#closemapbutton').click(function() {
+    $('#maplistitemthree').click(function() {
         $('#myMap').animate({
             opacity: '.3'
         });
-		$('#myMap').animate({
+    $('#myMap').animate({
             left: '-2000px'
         });
-		$('#myMap').animate({
+    $('#myMap').animate({
             opacity: '0'
         });
-		$('video').removeClass('videostop');
-		$('html').removeClass('htmlbodycolor');
-		$('.controlsoff').removeClass('controlson');
-		$('.citation').removeClass('citationoff');
-		$('.mymapoff').removeClass('mymapon');
+    $('video').removeClass('videostop');
+    $('html').removeClass('htmlbodycolor');
+    $('.controlsoff').removeClass('controlson');
+    $('.citation').removeClass('citationoff');
+    $('.mymapoff').removeClass('mymapon');
     });
 };
 
