cd5adb09d961ab648c4d62b0a0036db3156b0085
Andy Klimczak <andyklimczak@fastmail.com>
2015-12-31 13:15:12 -0500

    User model test validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, :type => :model do
     user = create(:user)
     expect(user.save).to eq(true)
   end
+
+  it 'cannot have the same username as another users email' do
+    user1 = create(:user, email: 'email@test.org')
+    user2 = build(:user, username: 'email@test.org')
+    expect(user2).not_to be_valid
+  end
 end
