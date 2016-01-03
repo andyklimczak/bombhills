48f3096115dfec41fdd42426b3b3e59e5c8ee715
Andy Klimczak <andyklimczak@fastmail.com>
2016-01-03 16:52:32 -0500

    Sign in feature test tests both username and email signin

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "the signin process", :type => :feature do
   before :each do
-    create(:user, :email => 'user@example.com', :password => 'password123')
+    create(:user,username: 'user123', email: 'user@example.com', password: 'password123')
   end
 
-  it "signs me in and redirects to home page" do
+  it "signs me in with email and redirects to home page" do
     visit '/users/sign_in'
     within("#new_user") do
       fill_in 'Login', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password123'
+    end
+    click_button 'Log in'
+    expect(page).to have_current_path(root_path)
+  end
+
+  it "signs me in with username and redirects to home page" do
+    visit '/users/sign_in'
+    within("#new_user") do
+      fill_in 'Login', :with => 'user123'
       fill_in 'Password', :with => 'password123'
     end
     click_button 'Log in'
