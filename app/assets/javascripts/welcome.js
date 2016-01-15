3453a69fa278da61725b536d0022a36da6f7fb4b
alecs36 <alecs36@yahoo.com>
2016-01-15 02:46:11 -0500

    Added: mobile header w/ login, dashboard specific location search bar and button for full screen mode

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,16 @@
 function imageScroll() {
     var header = document.getElementById("header");
     var speed = 5.0;
+    var width = window.innerWidth;
 
     window.onscroll = function() {
-       var yOffset = window.pageYOffset;
-       header.style.backgroundPosition = "0px "+ (yOffset / speed) + "px";
+        if (width > 565px) {
+            var yOffset = window.pageYOffset;
+            header.style.backgroundPosition = "0px "+ (yOffset / speed) + "px";
+        }
+       else {
+           
+       }
     }
 };
 
