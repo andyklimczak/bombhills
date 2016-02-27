705053af139b09cf34b52efdd8afd0b0aadb0788
Andy Klimczak <andyklimczak@fastmail.com>
2016-02-27 16:44:51 -0500

    Remove post seed data until better way to seed data is found

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.create(username: FFaker::Internet.user_name, email: FFaker::Internet.safe_e
   User.last.spots.create({title: FFaker::Venue.name, description: FFaker::HipsterIpsum.sentence, latitude: FFaker::Geolocation.lat, longitude: FFaker::Geolocation.lng, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample})
 end
 
-5.times do |i|
-  User.first.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: "http://placeskull.com/400/200/", spot: Spot.order("RANDOM()").first})
-  User.last.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: "http://placeskull.com/400/200/", spot: Spot.order("RANDOM()").first})
-end
+#5.times do |i|
+  #User.first.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: "http://placeskull.com/400/200/", spot: Spot.order("RANDOM()").first})
+  #User.last.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: "http://placeskull.com/400/200/", spot: Spot.order("RANDOM()").first})
+#end
