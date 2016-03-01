1fc9e6453512599e696b6a5cb394d2e8cfa577a1
alecs36 <alecs36@yahoo.com>
2016-02-29 22:48:46 -0500

    fix js to include check for element id header

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,13 +1,16 @@
 
 function imageScroll() {
-    var header = document.getElementById("header");
-    var speed = 5.0;
-    var width = window.innerWidth;
-
-    window.onscroll = function() {
-        var yOffset = window.pageYOffset;
-        header.style.backgroundPosition = "0px "+ (yOffset / speed) + "px";
-
+    if(document.getElementById("header")) {
+        var header = document.getElementById("header");
+        var speed = 5.0;
+        var width = window.innerWidth;
+        window.onscroll = function() {
+            var yOffset = window.pageYOffset;
+            header.style.backgroundPosition = "0px "+ (yOffset / speed) + "px";
+        }
+    }
+    else {
+        
     }
 };
 
