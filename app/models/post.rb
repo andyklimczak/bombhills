46483a6495a814d9b8312159acc63e95172a074e
Andy Klimczak <andyklimczak@fastmail.com>
2016-03-13 14:55:49 -0400

    Placeholder image for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,7 +23,8 @@
 #
 
 class Post < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://placehold.it/150x150"
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   belongs_to :user
   has_one :spot
