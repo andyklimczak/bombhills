ba1030934e5ba6e55a910229d902800ce3619d7d
Andy Klimczak <andyklimczak@fastmail.com>
2016-03-20 12:46:57 -0400

    Force square profile pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :login
-  has_attached_file :avatar, styles: { large: "800x800", medium: "300x300>", thumb: "100x100>" }, default_url: "https://placehold.it/500x500"
+  has_attached_file :avatar, styles: { large: "800x800#", medium: "300x300>", thumb: "100x100>" }, default_url: "https://placehold.it/500x500"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_many :spots, -> { order 'created_at desc' }
   has_many :posts, -> { order 'created_at desc' }
