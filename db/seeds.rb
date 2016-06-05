29f6e8394fa3ea88051aedccc74ad604f92b3c5f
Andy Klimczak <andyklimczak@fastmail.com>
2016-06-05 14:15:21 -0400

    Update seed data to create posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'factory_girl_rails'
 
 User.create(username: FFaker::Internet.user_name, email: FFaker::Internet.safe_email, password: FFaker::Internet.password, motto: FFaker::DizzleIpsum.sentence)
 User.create(username: FFaker::Internet.user_name, email: FFaker::Internet.safe_email, password: FFaker::Internet.password, motto: FFaker::DizzleIpsum.sentence)
 
-5.times do |i|
+10.times do |i|
   User.first.spots.create({title: FFaker::Venue.name, description: FFaker::HipsterIpsum.sentence, latitude: FFaker::Geolocation.lat, longitude: FFaker::Geolocation.lng, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample})
   User.last.spots.create({title: FFaker::Venue.name, description: FFaker::HipsterIpsum.sentence, latitude: FFaker::Geolocation.lat, longitude: FFaker::Geolocation.lng, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample})
 end
 
-#5.times do |i|
-  #User.first.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: "http://placeskull.com/400/200/", spot: Spot.order("RANDOM()").first})
-  #User.last.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: "http://placeskull.com/400/200/", spot: Spot.order("RANDOM()").first})
-#end
+2.times do |i|
+  User.first.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: FFaker::Avatar.image, spot: Spot.order("RANDOM()").first, type: "ImagePost"})
+  User.last.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: FFaker::Avatar.image, spot: Spot.order("RANDOM()").first, type: "ImagePost"})
+  User.last.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: FFaker::Avatar.image, type: "ImagePost"})
+  User.first.posts.create({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, video_url: 'https://www.youtube.com/watch?v=e-ORhEE9VVg', spot: Spot.order("RANDOM()").first, type: "VideoPost"})
+end
