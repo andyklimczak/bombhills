dcf8a15b2a695a73c063be6b341aac0e60345335
Andy Klimczak <andyklimczak@fastmail.com>
2016-06-05 19:01:18 -0400

    Message tests

---
+++ spec/features/messaging_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe "mailboxer messaging", :type => :feature do
+
+  it "can send a message" do
+    user = create(:user)
+    user2 = create(:user)
+    login_as user
+    visit new_conversation_path
+
+    chosen_select(user2.username, from: :conversation_recipients)
+    fill_in 'Subject', :with => 'Message Subject'
+    fill_in :conversation_body, :with => 'Message body to user2'
+    click_on 'Send Message'
+
+    expect(user.mailbox.sentbox.count).to eq(1)
+    expect(user2.mailbox.inbox.count).to eq(1)
+  end
+
+  it "can send a message to multiple recipients" do
+    user = create(:user)
+    user2 = create(:user)
+    user3 = create(:user)
+    login_as user
+    visit new_conversation_path
+
+    chosen_select(user2.username, user3.username, from: :conversation_recipients)
+    fill_in 'Subject', :with => 'Message Subject'
+    fill_in :conversation_body, :with => 'Message body to user2'
+    click_on 'Send Message'
+
+    expect(user.mailbox.sentbox.count).to eq(1)
+    expect(user2.mailbox.inbox.count).to eq(1)
+    expect(user3.mailbox.inbox.count).to eq(1)
+  end
+
+  it "can reply to a message" do
+    user = create(:user)
+    user2 = create(:user)
+    login_as user
+    user2.send_message(user, 'test body', 'test subject')
+    visit mailbox_inbox_path
+    expect(user.mailbox.inbox.count).to eq(1)
+    expect(user2.mailbox.sentbox.count).to eq(1)
+    click_on 'View'
+    fill_in :message_body, with: 'Test reply message'
+    click_on 'Reply'
+    expect(user2.mailbox.inbox.count).to eq(1)
+  end
+
+  it "can delete a message" do
+    user = create(:user)
+    user2 = create(:user)
+    login_as user
+    user2.send_message(user, 'test body', 'test subject')
+    visit mailbox_inbox_path
+    expect(user.mailbox.inbox.count).to eq(1)
+    expect(user2.mailbox.sentbox.count).to eq(1)
+    click_on 'View'
+    click_on 'Move to trash'
+    expect(user.mailbox.trash.count).to eq(1)
+  end
+end
