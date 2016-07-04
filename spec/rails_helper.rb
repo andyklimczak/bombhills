655b535b780bb7b2ec1f18d7b8d6570644ff9927
Andy Klimczak <andyklimczak@fastmail.com>
2016-07-03 23:05:54 -0400

    Update devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,8 +60,8 @@ RSpec.configure do |config|
 		Warden.test_mode!
 	end
 
-	config.include Devise::TestHelpers, type: :controller
-	config.include Devise::TestHelpers, type: :view
+	config.include Devise::Test::ControllerHelpers, type: :controller
+	config.include Devise::Test::ControllerHelpers, type: :view
 
 	config.include FactoryGirl::Syntax::Methods
 
