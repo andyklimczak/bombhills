ddc43448b0c8d4070039d14059115a1da4d5b2fa
Andy Klimczak <andyklimczak@fastmail.com>
2016-08-17 22:23:40 -0400

    remove hard coded values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,6 @@ Rails.application.configure do
 		:address              => "smtp.gmail.com",
 		:port                 => 587,
 		:domain               => "gmail.com",
-		:user_name            => "bombhillscommunity@gmail.com",
-		:password             => "bombasshills",
 		:user_name            => ENV['EMAIL_USERNAME'],
 		:password             => ENV['EMAIL_PASSWORD'],
 		:authentication       => :login,
