a040041abe4af6e593f6fb7aea1cf64612cef808
alecs36 <alecs36@yahoo.com>
2016-08-19 01:28:42 -0400

    Okay i think i'm done for thei night. Just rebranded every page (for the most part). Our website shold have consistent font usage all around (Google font: Kanit) now, and i'm using a them of 3 different colors: #fb4148, #0d003d and #0B4CD6. I am using variables right now just so i can easily change colors around, i found and replaced all back to standard hexadecimals for this push so we can see everything across browsers. I figured out what you meant by how the way the meetups pages are currently set up and will complete those designs tomorrow. I'm not thinking anything fancy just how the signup / login pages are, simple data in the middle of each page. I just want to keep it simple for the user for now, i'm assuming this will be best/easiest for on the go mobile users.

--- app/assets/javascripts/spots.js
+++ app/assets/javascripts/spots.js
@@ -77,7 +77,7 @@ function loadPoints(e) {
   }).done(function(response) {
     response.spots.forEach(function(spot) {
       L.marker([spot.latitude, spot.longitude], { clickable: true })
-        .bindPopup("<b>" + spot.title + "</b></br>Description: " + spot.description + "<br>Created by: " + spot.user.username + "<br>Difficulty: " + spot.difficulty + "<br>Traffic: " + spot.traffic + "<br><a href='/spots/" + spot.id + "/meetups/new'>meetup</a>" +"<br><a data-toggle='modal' data-target='#reportModal'>report</a>")
+        .bindPopup("<b>" + spot.title + "</b></br>Description: " + spot.description + "<br>Created by: " + spot.user.username + "<br>Difficulty: " + spot.difficulty + "<br>Traffic: " + spot.traffic + "<br><a href='/spots/" + spot.id + "/meetups'>meetup</a>" +"<br><a data-toggle='modal' data-target='#reportModal'>report</a>")
         .addTo(mymap);
     });
   }).fail(function() {
