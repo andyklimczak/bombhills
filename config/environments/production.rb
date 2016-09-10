129b235625afa9c6b38dc5f03d2d55e7f048b942
Andy Klimczak <andyklimczak@fastmail.com>
2016-09-10 16:47:46 -0400

    Attempt to fix email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
 		:domain               => "gmail.com",
 		:user_name            => ENV['EMAIL_USERNAME'],
 		:password             => ENV['EMAIL_PASSWORD'],
-		:authentication       => :plain,
+		:authentication       => :login,
 		:enable_starttls_auto => true
 	}
 end
