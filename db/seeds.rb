b9d95b76617d9130956e1793c42ef15fa5cd55c7
alecs36 <alecs36@yahoo.com>
2016-09-11 23:43:47 -0400

    start working on preloading map spots

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,4 +20,15 @@ end
 end
 
 admin = User.create(username: 'admin', email: 'bombhillscommunity@gmail.com', password: 'bombhills', motto: 'bomb ass hills')
-admin.spots.create({title: 'test', description: 'test', latitude: 37.779127216982424, longitude: -122.46700286865234, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample, traffic: 'test'})
+
+admin.spots.create({title: 'Geary St. SF', description: 'Night ride, timed street lights. Nice wide carves.', latitude: 37.77948336934459, longitude: -122.49653398990631, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample, traffic: 'na'})
+
+admin.spots.create({title: 'Rivera St.', description: 'Nice cruiser hill w a killer view of the ocean.', latitude: 37.74578121450763, longitude: -122.49834179878236, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample, traffic: 'na'})
+
+admin.spots.create({title: 'Lombard street', description: 'Anthology!!! Downhill sliding. Brick pavement.', latitude: 37.801989564915104, longitude: -122.41952776908873, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample, traffic: 'Bad'})
+
+admin.spots.create({title: 'Mclaren Park', description: 'the SF Gravity fest cource', latitude: 37.72350400727579, longitude: -122.42274641990662, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample, traffic: 'na'})
+
+admin.spots.create({title: 'Muir beach run', description: 'dope view but dont fall off the edge', latitude: 37.88052730149224, longitude: -122.55414247512817, difficulty: ['Beginner', 'Intermediate', 'Expert'].sample, traffic: 'na'})
+
+
