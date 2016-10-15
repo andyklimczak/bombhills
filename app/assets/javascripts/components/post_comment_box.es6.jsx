1883b2d6c7e51d51ff4ccf39bbee053f571466f1
Andy Klimczak <andyklimczak@fastmail.com>
2016-10-15 18:47:38 -0400

    clear input on submit

--- app/assets/javascripts/components/post_comment_box.es6.jsx
+++ app/assets/javascripts/components/post_comment_box.es6.jsx
@@ -1,10 +1,17 @@
 class PostCommentBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      body: ''
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({body: e.target.value});
   }
   handleSubmit(e) {
-    const data = { comment: { post_id: this.props.postId, body: this.refs.body.value } }
+    const data = { comment: { post_id: this.props.postId, body: this.state.body } }
     $.ajax({
       method: 'POST',
       url: '/comments',
@@ -13,12 +20,13 @@ class PostCommentBox extends React.Component {
       data: JSON.stringify(data)
     }).done((comment) => {
       this.props.handleSubmit(comment);
+      this.setState({body: ''});
     });
   }
   render () {
     return (
       <div className="comment-box">
-        <textarea rows="5" cols="50" type="text" ref="body" />
+        <textarea rows="5" cols="50" type="text" ref="body" value={this.state.body} onChange={this.handleChange} />
         <a className="btn btn-primary btn-submit" onClick={this.handleSubmit}>
           Submit
         </a>
