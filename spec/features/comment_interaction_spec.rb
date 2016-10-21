82a77fcc3639523c5add46c981bc03f70dc89bbc
Andy Klimczak <andyklimczak@fastmail.com>
2016-10-21 10:49:14 -0400

    more tests

---
+++ spec/features/comment_interaction_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'comment interactions', type: :feature do
+  xit 'can be posted from the spot page' do
+  end
+
+  xit 'can be deleted from the spot page' do
+  end
+
+  xit 'can be posted from the post page' do
+  end
+
+  xit 'can be deleted from the post page' do
+  end
+
+  xit 'can go to comment user page by clickin on username' do
+  end
+end
