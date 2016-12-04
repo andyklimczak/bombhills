6187eab0e816057549ebf9fccfe6f8ed1887976d
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-04 01:26:00 -0500

    requests comments spec rewrite

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,63 +2,70 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
+  let(:headers) { {
+    'ACCEPT' => 'application/json',
+    'HTTP_ACCEPT' => 'application/json'
+  } }
+  let(:spot) { create(:spot) }
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
   describe 'POST /comments', type: :request do
-    it 'creates comment for spot' do
-      headers = {
-        'ACCEPT' => 'application/json',
-        'HTTP_ACCEPT' => 'application/json'
-      }
-      sign_in create(:user)
-      spot = create(:spot)
-      post('/comments', params: { comment: { commentable_type: spot.class.to_s, commentable_id: spot.id, body: 'message' } }, headers: headers)
-      spot.reload
-      expect(spot.comment_threads.count).to eq(1)
-      expect(response.status).to eq(200)
+    describe'creates comment for spot' do
+      before do
+        post('/comments', params: { comment: { commentable_type: spot.class.to_s, commentable_id: spot.id, body: 'message' } }, headers: headers)
+        spot.reload
+      end
+
+      it 'increases thread count' do
+        expect(spot.comment_threads.count).to eq(1)
+      end
+
+      it 'good status' do
+        expect(response.status).to eq(200)
+      end
     end
 
-    it 'requires message body' do
-      headers = {
-        'ACCEPT' => 'application/json',
-        'HTTP_ACCEPT' => 'application/json'
-      }
-      sign_in create(:user)
-      spot = create(:spot)
-      post('/comments', params: { comment: { commentable_type: spot.class.to_s, commentable_id: spot.id, body: nil } }, headers: headers)
-      spot.reload
-      expect(spot.comment_threads.count).to eq(0)
-      expect(response.status).to eq(422)
+    describe 'requires message body' do
+      before do
+        post('/comments', params: { comment: { commentable_type: spot.class.to_s, commentable_id: spot.id, body: nil } }, headers: headers)
+        spot.reload
+      end
+
+      it 'does not increase thread count' do
+        expect(spot.comment_threads.count).to eq(0)
+      end
+
+      it 'bad status' do
+        expect(response.status).to eq(422)
+      end
     end
   end
 
   describe 'DELETE /comments', type: :request do
-    it 'deletes comment' do
-      headers = {
-        'ACCEPT' => 'application/json',
-        'HTTP_ACCEPT' => 'application/json'
-      }
-      user = create(:user)
-      sign_in user
-      spot = create(:spot)
-      comment = create(:comment, commentable: spot, user: user)
-      expect(spot.comment_threads.count).to eq(1)
-      delete("/comments/#{comment.id}", headers: headers)
-      spot.reload
-      expect(spot.comment_threads.count).to eq(0)
+
+    describe 'deletes comment' do
+      let(:comment) { create(:comment, commentable: spot, user: user) }
+
+      it 'deletes' do
+        delete("/comments/#{comment.id}", headers: headers)
+        spot.reload
+        expect(spot.comment_threads.count).to eq(0)
+      end
     end
 
-    it 'cannot be deleted by other user' do
-      headers = {
-        'ACCEPT' => 'application/json',
-        'HTTP_ACCEPT' => 'application/json'
-      }
-      user1 = create(:user)
-      user2 = create(:user)
-      sign_in user2
-      spot = create(:spot, user: user1)
-      comment = create(:comment, commentable: spot, user: user1)
-      expect(spot.comment_threads.count).to eq(1)
-      delete("/comments/#{comment.id}", headers: headers)
-      expect(spot.comment_threads.count).to eq(1)
+    describe 'cannot be deleted by other user' do
+      let(:user2) { create(:user) }
+      let(:comment) { create(:comment, commentable: spot, user: user) }
+
+      it 'does not delete' do
+        sign_in user2
+        delete("/comments/#{comment.id}", headers: headers)
+        expect(spot.comment_threads.count).to eq(1)
+      end
     end
   end
 end
