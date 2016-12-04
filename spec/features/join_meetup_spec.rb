b10ad8965b21ed6a56a0b7dd10c231fb612a6fa9
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-03 23:26:18 -0500

    rewrite join/leave meetup spec

--- spec/features/join_meetup_spec.rb
+++ spec/features/join_meetup_spec.rb
@@ -1,28 +1,45 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe 'join meetup', type: :feature, js: true do
+RSpec.describe 'join/leave meetup', type: :feature, js: true do
+  let(:user) { create(:user) }
+  let(:spot) { create(:spot) }
+  let(:meetup) { create(:meetup, user: user, spot: spot) }
+
   before do
-    @user = create(:user)
-    @spot = create(:spot)
-    @meetup = create(:meetup, user: @user, spot: @spot)
-    login_as @user, scope: :user
+    login_as user, scope: :user
   end
 
-  it 'join a meetup' do
-    visit spot_meetup_path(@spot, @meetup)
-    find('.join-meetup').click
-    wait_for_ajax
-    expect(@meetup.attending_users.count).to eq(1)
-    expect(@user.attending_meetups.count).to eq(1)
+  describe 'join a meetup' do
+    before do
+      visit spot_meetup_path(spot, meetup)
+      find('.join-meetup').click
+      wait_for_ajax
+    end
+
+    it 'increases meetup attending users' do
+      expect(meetup.attending_users.count).to eq(1)
+    end
+
+    it 'increases user attending meetups' do
+      expect(user.attending_meetups.count).to eq(1)
+    end
   end
 
-  it 'leave a meetup' do
-    create(:meetup_attendee, user: @user, meetup: @meetup)
-    visit spot_meetup_path(@spot, @meetup)
-    find('.leave-meetup').click
-    wait_for_ajax
-    expect(@meetup.attending_users.count).to eq(0)
-    expect(@user.attending_meetups.count).to eq(0)
+  describe 'leave a meetup' do
+    before do
+      create(:meetup_attendee, user: user, meetup: meetup)
+      visit spot_meetup_path(spot, meetup)
+      find('.leave-meetup').click
+      wait_for_ajax
+    end
+
+    it 'decreases meetup attending users' do
+      expect(meetup.attending_users.count).to eq(0)
+    end
+
+    it 'decreases user attending meetups' do
+      expect(user.attending_meetups.count).to eq(0)
+    end
   end
 end
