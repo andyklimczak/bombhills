cf6680ccbd7781e1487b9583b01419c1f796bff4
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-04 00:42:27 -0500

    rewrite message mailer spec

--- spec/mailers/message_mailer_spec.rb
+++ spec/mailers/message_mailer_spec.rb
@@ -2,39 +2,45 @@
 require 'rails_helper'
 
 RSpec.describe MessageMailer, type: :mailer do
-  before(:all) do
-    msg = Message.new(name: 'Test Name', subject: 'Test Subject', content: 'Test Content', email: 'test@email.com')
-    @email = MessageMailer.message_me(msg).deliver_now
-    @confirmation_email = MessageMailer.message_me_confirmation(msg).deliver_now
-  end
+    let(:msg) { Message.new(name: 'Test Name', subject: 'Test Subject', content: 'Test Content', email: 'test@email.com') }
+    let(:email) { MessageMailer.message_me(msg).deliver_now }
+    let(:confirmation_email) { MessageMailer.message_me_confirmation(msg).deliver_now }
 
   describe 'message mailer' do
     it 'renders the subject' do
-      expect(@email.subject).to eq('Test Subject')
+      expect(email.subject).to eq('Test Subject')
     end
 
-    it 'renders the message' do
-      expect(@email.body.encoded).to match('Test Name')
-      expect(@email.body.encoded).to match('Test Content')
+    context 'renders the message' do
+      it 'renders username in body' do
+        expect(email.body.encoded).to match('Test Name')
+      end
+      it 'render content in body' do
+        expect(email.body.encoded).to match('Test Content')
+      end
     end
 
     it 'renders the email' do
-      expect(@email.from).to eq(['test@email.com'])
+      expect(email.from).to eq(['test@email.com'])
     end
   end
 
   describe 'confirmation message' do
     it 'renders the subject' do
-      expect(@confirmation_email.subject).to eq('Test Subject')
+      expect(confirmation_email.subject).to eq('Test Subject')
     end
 
-    it 'renders the message' do
-      expect(@confirmation_email.body.encoded).to match('Test Name')
-      expect(@confirmation_email.body.encoded).to match('Test Content')
+    context 'renders the message' do
+      it 'renders username in body' do
+        expect(email.body.encoded).to match('Test Name')
+      end
+      it 'render content in body' do
+        expect(email.body.encoded).to match('Test Content')
+      end
     end
 
     it 'renders the email' do
-      expect(@confirmation_email.to).to eq(['test@email.com'])
+      expect(confirmation_email.to).to eq(['test@email.com'])
     end
   end
 end
