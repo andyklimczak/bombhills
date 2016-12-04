93594c50763013c4817700102e1adbf91e093ca0
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-04 00:46:31 -0500

    rewrite signup mailer

--- spec/mailers/signup_mailer_spec.rb
+++ spec/mailers/signup_mailer_spec.rb
@@ -2,26 +2,22 @@
 require 'rails_helper'
 
 RSpec.describe SignupMailer, type: :mailer do
-  before(:all) do
-    @user = create(:user)
-    @mail = SignupMailer.signup_notification(@user).deliver_now
+  let(:user) { create(:user) }
+  let(:mail) { SignupMailer.signup_notification(user).deliver_now }
+
+  it 'renders the subject' do
+    expect(mail.subject).to eq('You signed up')
   end
 
-  describe 'signup mailer' do
-    it 'renders the subject' do
-      expect(@mail.subject).to eq('You signed up')
-    end
+  it 'renders the receiver email' do
+    expect(mail.to).to eq([user.email])
+  end
 
-    it 'renders the receiver email' do
-      expect(@mail.to).to eq([@user.email])
-    end
+  it 'renders the sender email' do
+    expect(mail.from).to eq(['notifications@bombhills.com'])
+  end
 
-    it 'renders the sender email' do
-      expect(@mail.from).to eq(['notifications@bombhills.com'])
-    end
-
-    it 'assigns @user' do
-      expect(@mail.body.encoded).to match(@user.name)
-    end
+  it 'assigns user' do
+    expect(mail.body.encoded).to match(user.name)
   end
 end
