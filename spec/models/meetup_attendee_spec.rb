2cac508c37e66615f1bb88f4423031c55166719d
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-04 01:10:37 -0500

    meetup attendee spec

--- spec/models/meetup_attendee_spec.rb
+++ spec/models/meetup_attendee_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 
 RSpec.describe MeetupAttendee, type: :model do
   it 'can be created' do
-    meetup_attendee = create(:meetup_attendee)
-    expect(meetup_attendee.save).to eq(true)
+    expect(create(:meetup_attendee).save).to eq(true)
   end
 
   it { is_expected.to belong_to :user }
