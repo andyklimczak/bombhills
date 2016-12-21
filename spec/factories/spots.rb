20768dc3731875a82c6ba0587f209c8a7ff67c43
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-21 01:49:10 -0500

    rubocop

--- spec/factories/spots.rb
+++ spec/factories/spots.rb
@@ -3,7 +3,7 @@ require 'ffaker'
 
 FactoryGirl.define do
   factory :spot, aliases: [:commentable] do
-    title { FFaker::Venue.name + Random.rand(10_000).to_s  }
+    title { FFaker::Venue.name + Random.rand(10_000).to_s }
     traffic { FFaker::HipsterIpsum.phrase }
     description { FFaker::HipsterIpsum.sentence }
     latitude { FFaker::Geolocation.lat }
