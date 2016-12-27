0a7c60fc0055c5cb1cf3dd9c04cdef24bb13e33e
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-27 16:58:27 -0500

    cleanup meetup form

--- app/assets/javascripts/meetups.js
+++ app/assets/javascripts/meetups.js
@@ -1,9 +1,8 @@
 function initDateTimePicker() {
-  console.log('init');
   if($('#meetup-datetime-picker').length) {
     $('#meetup-datetime-picker').datetimepicker({
       minDate: moment().add(1, 'day'),
-      sideBySide: true
+      sideBySide: true,
     });
   }
 }
