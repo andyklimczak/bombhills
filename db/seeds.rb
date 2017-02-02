c0e681461fa60b5658d65c344adf3445044c177d
Andy Klimczak <andyklimczak@fastmail.com>
2017-02-02 14:44:59 -0500

    remove dummy data in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,3 @@
-until User.count > 2 do
-  User.create!(username: FFaker::Name.first_name, email: FFaker::Internet.safe_email, password: FFaker::Internet.password, motto: FFaker::DizzleIpsum.sentence)
-  User.create!(username: FFaker::Name.first_name, email: FFaker::Internet.safe_email, password: FFaker::Internet.password, motto: FFaker::DizzleIpsum.sentence)
-end
-
-3.times do |i|
-  User.first.spots.create!({title: FFaker::Venue.name, description: FFaker::HipsterIpsum.sentence, latitude: FFaker::Geolocation.lat, longitude: FFaker::Geolocation.lng, difficulty: ['Beginner', 'Intermediate', 'Professional'].sample, traffic: FFaker::HipsterIpsum.phrase})
-  User.last.spots.create!({title: FFaker::Venue.name, description: FFaker::HipsterIpsum.sentence, latitude: FFaker::Geolocation.lat, longitude: FFaker::Geolocation.lng, difficulty: ['Beginner', 'Intermediate', 'Professional'].sample, traffic: FFaker::HipsterIpsum.phrase})
-end
-
-3.times do |i|
-  User.first.posts.create!({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: FFaker::Avatar.image, spot: Spot.order("RANDOM()").first, type: "ImagePost"})
-  User.last.posts.create!({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: FFaker::Avatar.image, spot: Spot.order("RANDOM()").first, type: "ImagePost"})
-  User.last.posts.create!({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image: FFaker::Avatar.image, type: "ImagePost"})
-  User.first.posts.create!({title: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, video_url: FFaker::Youtube.url, spot: Spot.order("RANDOM()").first, type: "VideoPost"})
-end
-
-4.times do |i|
-  Spot.first.meetups.create!({title: FFaker::Movie.title, description: FFaker::Lorem.sentence, user: User.first, time: FFaker::Time.date})
-end
-
 admin = User.create!(username: 'admin', email: 'bombhillscommunity@gmail.com', password: 'bombhills', motto: 'bomb ass hills')
 
 # san fran spots
