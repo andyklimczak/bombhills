26d7e1958d96c5a9da6f545b199b026e256c9979
Andy Klimczak <andyklimczak@fastmail.com>
2017-02-18 17:17:35 -0500

    add can_edit helper for resources

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def active_page(active_page)
     @active == active_page ? 'active' : ''
   end
+
+  def can_edit(user, resource)
+    user == resource.user
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  let(:user) { create(:user) }
+  let(:user_meetup) { create(:meetup, user: user) }
+  let(:meetup) { create(:meetup) }
+
+  describe 'can edit resource' do
+    it 'can edit resource' do
+      expect(helper.can_edit(user, user_meetup)).to be true
+    end
+
+    it 'cannot edit resource' do
+      expect(helper.can_edit(user, meetup)).to be false
+    end
+  end
+end
