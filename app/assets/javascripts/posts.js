41641a679f62eda29de44c75a3efb688ba1e2677
Andy Klimczak <andyklimczak@fastmail.com>
2017-03-19 00:44:40 -0400

    alternate inf scroll - breaks tests

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,54 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function initPostScroll() {
+  let content = $('#posts-container');    // where to load new content
+  let viewMore = $('#view-more'); // tag containing the "View More" link
+
+  let isLoadingNextPage = false;  // keep from loading two pages at once
+  let lastLoadAt = null;          // when you loaded the last page
+  let minTimeBetweenPages = 1000; // milliseconds to wait between loading pages
+  let loadNextPageAt = 500;      // pixels above the bottom
+
+  let waitedLongEnoughBetweenPages = () => (lastLoadAt === null) || ((new Date() - lastLoadAt) > minTimeBetweenPages);
+
+  let approachingBottomOfPage = function() {
+    let viewMore = $('#view-more'); // tag containing the "View More" link
+    if(viewMore.length < 1) { return false; }
+    return (document.documentElement.clientHeight + $(document).scrollTop()) > (content.height() - loadNextPageAt);
+  }
+
+  let nextPage = function() {
+    let url = viewMore.find('a').attr('href');
+
+    if (isLoadingNextPage || !url) { return; }
+
+    viewMore.addClass('loading');
+    isLoadingNextPage = true;
+    lastLoadAt = new Date();
+
+    return $.ajax({
+      url,
+      method: 'GET',
+      dataType: 'script',
+      success() {
+        viewMore.removeClass('loading');
+        isLoadingNextPage = false;
+        return lastLoadAt = new Date();
+      }
+    });
+  };
+
+  // watch the scrollbar
+  $(window).scroll(function() {
+    if (approachingBottomOfPage() && waitedLongEnoughBetweenPages()) {
+      return nextPage();
+    }
+  });
+
+  // failsafe in case the user gets to the bottom
+  // without infinite scrolling taking affect.
+  return viewMore.find('a').click(function(e) {
+    nextPage();
+    return e.preventDefault();
+  });
+}
+
+document.addEventListener('turbolinks:load', initPostScroll);
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(9)
   end
 
   # GET /posts/1
