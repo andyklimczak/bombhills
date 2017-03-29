26999a18abdaa6dd593e2ed3daf087f373158b8e
Andy Klimczak <andyklimczak@fastmail.com>
2017-03-28 20:19:15 -0400

    fix inf scroll failing tests

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,54 +1,57 @@
 function initPostScroll() {
-  let content = $('#posts-container');    // where to load new content
-  let viewMore = $('#view-more'); // tag containing the "View More" link
+  var content = $('#posts-container');    // where to load new content
+  if(content.length > 0) {
+    var viewMore = $('#view-more'); // tag containing the "View More" link
 
-  let isLoadingNextPage = false;  // keep from loading two pages at once
-  let lastLoadAt = null;          // when you loaded the last page
-  let minTimeBetweenPages = 1000; // milliseconds to wait between loading pages
-  let loadNextPageAt = 500;      // pixels above the bottom
+    var isLoadingNextPage = false;  // keep from loading two pages at once
+    var lastLoadAt = null;          // when you loaded the last page
+    var minTimeBetweenPages = 1000; // milliseconds to wait between loading pages
+    var loadNextPageAt = 500;      // pixels above the bottom
 
-  let waitedLongEnoughBetweenPages = () => (lastLoadAt === null) || ((new Date() - lastLoadAt) > minTimeBetweenPages);
+    var waitedLongEnoughBetweenPages = function() {
+      return (lastLoadAt === null) || ((new Date() - lastLoadAt) > minTimeBetweenPages);
+    };
 
-  let approachingBottomOfPage = function() {
-    let viewMore = $('#view-more'); // tag containing the "View More" link
-    if(viewMore.length < 1) { return false; }
-    return (document.documentElement.clientHeight + $(document).scrollTop()) > (content.height() - loadNextPageAt);
-  }
+    var approachingBottomOfPage = function() {
+      var viewMore = $('#view-more'); // tag containing the "View More" link
+      if(viewMore.length < 1) { return false; }
+      return (document.documentElement.clientHeight + $(document).scrollTop()) > (content.height() - loadNextPageAt);
+    }
 
-  let nextPage = function() {
-    let url = viewMore.find('a').attr('href');
+    var nextPage = function() {
+      var url = viewMore.find('a').attr('href');
 
-    if (isLoadingNextPage || !url) { return; }
+      if (isLoadingNextPage || !url) { return; }
 
-    viewMore.addClass('loading');
-    isLoadingNextPage = true;
-    lastLoadAt = new Date();
+      viewMore.addClass('loading');
+      isLoadingNextPage = true;
+      lastLoadAt = new Date();
 
-    return $.ajax({
-      url,
-      method: 'GET',
-      dataType: 'script',
-      success() {
+      return $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'script',
+      }).done(function() {
         viewMore.removeClass('loading');
         isLoadingNextPage = false;
         return lastLoadAt = new Date();
+      });
+    };
+
+    // watch the scrollbar
+    $(window).scroll(function() {
+      if (approachingBottomOfPage() && waitedLongEnoughBetweenPages()) {
+        return nextPage();
       }
     });
-  };
 
-  // watch the scrollbar
-  $(window).scroll(function() {
-    if (approachingBottomOfPage() && waitedLongEnoughBetweenPages()) {
-      return nextPage();
-    }
-  });
-
-  // failsafe in case the user gets to the bottom
-  // without infinite scrolling taking affect.
-  return viewMore.find('a').click(function(e) {
-    nextPage();
-    return e.preventDefault();
-  });
+    // failsafe in case the user gets to the bottom
+    // without infinite scrolling taking affect.
+    return viewMore.find('a').click(function(e) {
+      nextPage();
+      return e.preventDefault();
+    });
+  }
 }
 
 document.addEventListener('turbolinks:load', initPostScroll);
