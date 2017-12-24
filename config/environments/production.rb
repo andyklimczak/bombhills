e986bd3c63a6dfbf0efe866e57d61dc1b9106913
Andy Klimczak <andyklimczak@fastmail.com>
2017-12-23 19:04:00 -0500

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.sendgrid.net",
+    :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "heroku.com",
-    :user_name            => ENV['SENDGRID_USERNAME'],
-    :password             => ENV['SENDGRID_PASSWORD'],
+    :domain               => "gmail.com",
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => :plain,
     :enable_starttls_auto => true
   }
